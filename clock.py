a5d23ed9957502fe352a2d66afd0a2c00d2f8417
RavikumaR600 <67065363+RavikumaR600@users.noreply.github.com>
2021-05-26 18:15:17 +0530

    Update clock.py

--- clock.py
+++ clock.py
@@ -10,14 +10,14 @@ def times():
 
 root=Tk()
 root.geometry("500x250")
-clock=Label(root,font=("times",50,"bold"),bg="grey")
+clock=Label(root,font=("times",50,"bold"),bg="white")
 clock.grid(row=2,column=2,pady=25,padx=100)
 times()
 
-digi=Label(root,text="Digital clock",font="times 24 bold")
+digi=Label(root,text="Digital Clock",font="times 24 bold")
 digi.grid(row=0,column=2)
 
-nota=Label(root,text="hours   minutes   seconds   ",font="times 15 bold")
+nota=Label(root,text="Hours   Minutes   Seconds   ",font="times 15 bold")
 nota.grid(row=3,column=2)
 
 root.mainloop()
